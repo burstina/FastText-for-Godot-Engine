8d25aff61050ca170369d22aaba4ad4ce7632ddb
burstina <47447512+burstina@users.noreply.github.com>
2020-02-10 21:02:39 +0100

    Add files via upload

--- FastText/_FastText_IC_custom_script.gd
+++ FastText/_FastText_IC_custom_script.gd
@@ -1,38 +1,21 @@
-extends TextEdit
+extends Control
 
-func _process(delta):
-	_FastEditIC_process(delta)
-func _input(event):
-	_FastEditIC_input(event)
 
-#--# FastEdit Area ###################################################################
 var IC_OtherAlreadyOpenMeta 	 = "FastTextICAlreadyOpen"			# true/false
-var IC_OtherAlreadyOpenOwnerMeta = "FastTextICAlreadyOpenOwner"   	# object
-var MyOwner 	= null	# Calling FastEdit node
-var ResetCursor	= true
-var ResetMyOwner= true
-#--# FastEdit Area ###################################################################
+var IC_OtherAlreadyOpenOwnerMeta = "FastTextICAlreadyOpenOwner"   # object
+var MyOwner = null
 
-func _FastEditIC_process(delta):
-	if ResetMyOwner:
-		MyOwner =_get_IC_owner()
-	elif ResetCursor:
-		MyOwner._IC_reset_cursor()
-		
-func _FastEditIC_input(event):
-	if MyOwner != null:
-		if MyOwner._IC_input(event):
-			accept_event()
+func _ready():
+    MyOwner=_get_other_IC_AlreadyOpen_Owner()
+    pass
 
-func _get_IC_owner():  # fromt FastText.gd
-	ResetMyOwner = false
-	return get_tree().get_meta(IC_OtherAlreadyOpenOwnerMeta)
+func _input(event):
+    if MyOwner._IC_input(event):
+        accept_event()
 
-func _set_IC_owner(inp):  # fromt FastText.gd
-	MyOwner = inp
-	
-func _set_ResetCursor(flag=true):
-	ResetCursor = flag
-	
-		
-#--# FastEdit Area ###################################################################
+
+
+func _get_other_IC_AlreadyOpen_Owner():  # fromt FastText.gd
+    if get_tree().has_meta(IC_OtherAlreadyOpenOwnerMeta):
+        return get_tree().get_meta(IC_OtherAlreadyOpenOwnerMeta)
+    return null
